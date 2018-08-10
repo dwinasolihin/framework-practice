630ec1c02e88174a786000518495bb3e529bda19
Dwina Solihin <dwinasolihin@gmail.com>
2018-08-09 19:04:19 -0700

    Finished TODO 1

--- script.js
+++ script.js
@@ -16,3 +16,12 @@ var gators = [
     imgSource: "http://www.vaguebuttrue.com/images/1449775760-alligatorclimbsfenceWEBSITE.jpg"
   }
 ]
+var app;
+document.addEventListener("DOMContentLoaded", function(){
+  app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    }
+  })
+})
